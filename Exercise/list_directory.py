661e316d17e8deb979faec8157fb3638930bcc3e
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-08-31 02:00:50 +0530

    Changes in Exercise

--- Exercise/list_directory.py
+++ Exercise/list_directory.py
@@ -2,16 +2,16 @@ import os
 
 def print_directory_contents(directory):
     try:
-        # Get the list of all files and directories
+        # list all files and folders inside given directory
         items = os.listdir(directory)
         print(f"Contents of '{directory}':")
         for item in items:
             print(item)
-    except FileNotFoundError:
+    except FileNotFoundError:      # if directory path does not exist
         print(f"The directory '{directory}' does not exist.")
-    except PermissionError:
+    except PermissionError:        # if no permission to access folder
         print(f"Permission denied to access the directory '{directory}'.")
 
-# Example usage:
-directory_path = "."  # Current directory
+# Example usage
+directory_path = "."   # "." means current working directory
 print_directory_contents(directory_path)
