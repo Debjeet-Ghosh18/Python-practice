a997378221e19d5a6e4cf8559af0103a6e2d4ae5
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-09-02 00:03:11 +0530

    changes in String

---
+++ String/string_checks.py
@@ -0,0 +1,7 @@
+s = "Hello123"
+
+print("Is Alpha:", s.isalpha())   
+print("Is Digit:", s.isdigit())   
+print("Is Alphanumeric:", s.isalnum()) 
+print("Starts with He:", s.startswith("He")) 
+print("Ends with 3:", s.endswith("3"))   
