e4a0de3e63cfc566d38f1df8f7103c41da97e7e2
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-09-02 00:02:08 +0530

    changes in String

---
+++ String/string_concat_format.py
@@ -0,0 +1,6 @@
+name = "Debjeet"
+course = "Data Science"
+
+print("My name is " + name + " and I study " + course)   # concatenation
+print(f"My name is {name} and I study {course}")         # f-string
+print("My name is {} and I study {}".format(name, course)) # format()
