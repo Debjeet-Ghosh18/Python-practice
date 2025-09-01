65edee6d86fbd83df864aa5357159f35c7206c76
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-09-01 23:55:15 +0530

    changes in String

--- String/Slicing.py
+++ String/Slicing.py
@@ -14,3 +14,23 @@
 # nm="harrygdg"
 # print(len(nm))
 # print(nm[-4:-2])#yg
+name = "Debjeet"
+print(len(name))   # length of string
+
+fruit = "Mango"
+fruit1 = len(fruit)
+print("Mango is a", fruit1, "letter Word")
+
+# string slicing
+print(fruit[0:5])   # Mango → from index 0 to 4
+print(fruit[1:4])   # ang → index 1 to 3
+print(fruit[2:5])   # ngo → index 2 to 4
+
+# negative indexing slices
+print(fruit[-3:-1])   # ng  → index -3 to -2
+print(fruit[-2:-1])   # g   → only index -2
+print(fruit[-4:-2])   # an  → index -4 to -3
+
+nm = "harrygdg"
+print(len(nm))        # length = 8
+print(nm[-4:-2])      # yg → index -4 to -3
