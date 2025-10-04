d22ce4bc3407bb66486f8f449207ab14860cb174
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-04 20:10:35 +0530

    Update

--- Basics/variable.py
+++ Basics/variable.py
@@ -4,3 +4,15 @@ age = 20
 
 print(type(name))
 print(type(age))
+
+
+#Data Typess
+
+#String Data Type
+name0 = 'Debjeet'
+name1 = "Debjeet"
+name2 = '''Debjeet'''
+
+print(name0)
+print(name1)
+print(name2)
