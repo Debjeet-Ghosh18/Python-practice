8af4386b3218c2826f6e9a0c4d4ed30e752a7490
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-05 16:23:06 +0530

    Update

--- Basics/input.py
+++ Basics/input.py
@@ -1,3 +1,4 @@
-a = int(input("Enter a number:  "))
-print(a)
-
+name = input("What is you name?\n")
+age = int(input("What is you age?\n"))
+weight = float(input("What is your weight?\n"))
+print("My name is ",name,"and my age is",age,"and my weight is",weight)
