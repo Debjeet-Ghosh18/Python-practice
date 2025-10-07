3fdf7f79d4832f644dfdcdbe72392f8bbecbb8df
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-08 01:30:35 +0530

    Update

---
+++ Exercise/Principle_interest.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value of p: "))
+b = int(input("Enter the value of r: "))
+c = int(input("Enter the value of t: "))
+
+si =(a*b*c)/100
+
+print("The simple Interest is: ",si)
+
+
