ffca5c8d0135734553c5ebdf9a792d0c350b31a5
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-19 01:02:50 +0530

    Update

---
+++ Exercise/greatest_number.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the value of a :"))
+b = int(input("Enter the value of b :"))
+c = int(input("Enter the value of c :"))
+
+
+if(a>b and a>c):
+    print("A is the greatest number")
+elif(b>c):
+    print("B is the gratest number")
+else:
+    print("C is the greatest number")
+
+
