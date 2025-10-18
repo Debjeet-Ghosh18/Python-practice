8665d748730dd003ad7754f49d9e79de569ebd7e
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-19 00:55:27 +0530

    Update

---
+++ String & Funtions/nesting.py
@@ -0,0 +1,9 @@
+age = int(input("Enter Your Age :"))
+
+if(age>18):
+    if(age>100):
+        print("You can not Vote")
+    else:
+        print("You Can Vote")
+else:
+    print("You can not vote")
