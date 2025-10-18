b0776dfe44b288f99a317e6c62960916a4d610c8
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-18 20:18:14 +0530

    Update

---
+++ String & Funtions/funtions.py
@@ -0,0 +1,22 @@
+str = "myself Debjeet Ghosh, I am Pursuing my B.Sc in Data Science"
+
+# ENDSWITH FUNTION
+hello = str.endswith("ab")
+print(str.endswith("ab"))
+print(hello)
+
+#Captilize Functions
+helloo = str.capitalize()
+print(str.capitalize())
+print(helloo)
+
+# replaced funtions
+print(str.replace("e","a"))
+
+#Find Functions
+print(str.find("Ghosh"))
+print(str.find("mhghgkh"))
+
+# Count Funtion
+count = str.count("e")
+print(count)
