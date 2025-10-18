641f3f809558beb39438969c49cab0c749d1659e
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-18 20:28:02 +0530

    Update

---
+++ Exercise/StringAndFuntions.py
@@ -0,0 +1,9 @@
+# WAP to input users first name & Prints its Length
+
+name = input("Enter Your Name : ")
+print("The Length of you name is ",len(name))
+
+#WAP to find the occurance of '$' in a String
+
+dollar = "Hi it is a $ funtions $ is used for the currency parameter"
+print(dollar.count("$"))
