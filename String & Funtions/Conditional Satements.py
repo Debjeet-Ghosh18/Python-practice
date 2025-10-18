0b4847325dc0cca97ab5ada22122fcce13bfbe91
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-18 20:37:47 +0530

    Update

---
+++ String & Funtions/Conditional Satements.py
@@ -0,0 +1,18 @@
+age = int(input("Enter Your Age :"))
+
+if(age>18):
+    print("You can Vote")
+else:
+    print("You can not vote")
+
+
+color = input("Enter the color oof traffic light: ")
+
+if(color=="green"):
+    print("Go")
+elif(color=="yellow"):
+    print("Start")
+elif(color=="red"):
+    print("Stop")
+else:
+    print("Wrong Input")
