57a100ea969ac165310021b909d8aaab61634c12
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-19 12:41:53 +0530

    Update

---
+++ List and Tuple/Slicing Methods.py
@@ -0,0 +1,80 @@
+list = [5,3,4,1,2]
+
+list.append(6) ### APPEND FUNTIONS
+print(list)
+
+list.sort() ### SORT FUNTION IN ASCENDING ORDER
+print(list)
+
+list.sort(reverse=False) ### SORT FUNCTIONS IN ASENDING ORDER
+print(list)
+
+list.sort(reverse=True) ### SORT FUNCTIONS IN DESENDING ORDER
+print(list)
+
+list.reverse() ### REVERSE FUNTIONS
+print(list)
+
+list.remove(1) ### REMOVE FUNTIONS
+print(list)
+
+list.pop(0) ### REMOVE FUNCTIONS
+print(list)
+
+list.insert(0,"hello") ### INSERT FUNCTIONS
+print(list)
+
+list1 = ['b','a','f','p','g','d'] ### LIST WITH CHAR
+
+list1.sort()
+print(list1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
