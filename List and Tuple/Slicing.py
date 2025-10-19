d2936e2807b3951a822f5475f8ff37c390b60e19
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-19 11:45:28 +0530

    Update

---
+++ List and Tuple/Slicing.py
@@ -0,0 +1,8 @@
+num = [45,85,78,96,25]
+
+print(num[1:4])
+print(num[:4])
+print(num[1:])
+print(num[-4:])
+print(num[:])
+print(num[-5:-1])
---
+++ List and Tuple/basic.py
@@ -0,0 +1,23 @@
+marks =[12,45,14,56,86]
+print(marks)
+
+print(type(marks))
+print(len(marks))
+
+print(marks[0])
+print(marks[1])
+
+marks[0]=70
+print(marks)
+
+
+
+
+
+
+
+
+
+
+
+
