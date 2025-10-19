9680789e6edad92fb9a194883025e6c99630e543
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-20 00:38:19 +0530

    Update

---
+++ Exercise/list_task_3.py
@@ -0,0 +1,12 @@
+### WAP TO COUNT THE NUMBER OF STUDENTS WITH THE "A" GRADE IN THE FOLLOWING TUPLE
+
+grade = ("a","b","c","d","b","a","a","a","b","c")
+
+print(grade)
+print('The Count of Grade "A" is',grade.count("a"))
+
+### STORE THE VALUES IN A LIST & SORT THEM FROM "A" TO "D"
+
+grade1 = ["a","b","c","d","b","a","a","a","b","c"]
+grade1.sort()
+print(grade1)
