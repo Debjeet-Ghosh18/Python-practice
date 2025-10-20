dd30f13f76f375da02723c7ead858ad721026e77
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-20 10:53:09 +0530

    Update

---
+++ Dictionary and Set/dictionary_basic.py
@@ -0,0 +1,19 @@
+dic = {
+    "name" : "Debjeet Ghosh", # We can store Charecter
+    "subject": ["SQL","Power Bi","Python","Excel"], # We can store List
+    "topics" : ("Dictionary","Set"), # We can store Tuple
+    "age": 20, # We can store Integer
+    "CGPA": 8.54, # We can store Float
+    "Berozger": True  # We can store Boolean
+}
+
+print(dic)
+print(dic["name"])
+dic["name"]= "Jeet" # We Can ReAssign
+print(dic)
+
+
+dic["surname"] = "Ghosh" # We Can Add Also
+print(dic)
+
+
