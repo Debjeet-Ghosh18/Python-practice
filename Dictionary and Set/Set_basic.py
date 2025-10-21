d36bcf17831497f7fc0a0fe47c615f32c682e220
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-21 20:03:16 +0530

    Updating Dictionary and Set Folder

---
+++ Dictionary and Set/Set_basic.py
@@ -0,0 +1,13 @@
+collection = {1,2,3,4,5,6,1,2,1,2,1,2,1,"Hello","World",1.2,5.6}
+
+print(collection)
+print(len(collection))
+print(type(collection))
+
+
+print(list(collection))
+
+
+collection_list = list(collection)
+print(type(collection_list))
+print(collection_list[0])
--- Dictionary and Set/dictionarymethods.py
+++ Dictionary and Set/dictionarymethods.py
@@ -7,35 +7,37 @@ dic = {
     "Berozger": True
     }
 
-### keys methods(it is used to return all the key values)
+## keys methods(it is used to return all the key values)
 
-# print(dic.keys())
-# print(list(dic.keys()))
-# print(len(list(dic.keys())))
-# print(dic.keys())
+print(dic.keys())
+print(list(dic.keys()))
+print(len(list(dic.keys())))
+print(dic.keys())
 
-# ### Values methods it is used to return all the values in the dictionary
+### Values methods it is used to return all the values in the dictionary
 
-# print(dic.values())
-# print(tuple(dic.values()))
-# print(list(dic.values()))
+print(dic.values())
+print(tuple(dic.values()))
+print(list(dic.values()))
 
-## items method return all (key,values) as pair as tuple
+# items method return all (key,values) as pair as tuple
 
 print((dic.items()))
 print(list(dic.items()))
 pair = list(dic.items())
 print(pair[1])
 
-
-
 ### get method is used for returns key according to value
 
+print(dic["Name"])
+print(dic.get("Name"))
 
 
+### Update is used to insert specified items in the dictionaries
 
-
-
+update = {"college":"BGU","Roll No":"23UG106002"}
+dic.update(update)
+print(dic)
 
 
 
