689c525c532be46a53971987c6c4aa7d91d8d1b5
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-21 12:19:02 +0530

    Updating Dictionary and Set Folder

---
+++ Dictionary and Set/dictionarymethods.py
@@ -0,0 +1,14 @@
+dic = {
+    "Name" : "Debjeet Ghosh",
+    "Skills": ["SQL","Power Bi","Python","Excel"], 
+    "Topics" : ("Dictionary","Set"), 
+    "Age": 20, 
+    "CGPA": 8.54,
+    "Berozger": True
+    }
+
+print(dic.keys())
+print(list(dic.keys()))
+print(len(list(dic.keys())))
+print(dic.keys())
+print(dic)
