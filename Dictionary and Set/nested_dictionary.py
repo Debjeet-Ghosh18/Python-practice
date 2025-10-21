98ab21c8a64c44d3cae1853aae32a44d269c65c6
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-21 12:02:10 +0530

    Updating Dictionary and Set Folder

---
+++ Dictionary and Set/nested_dictionary.py
@@ -0,0 +1,14 @@
+Studentdetails = {
+    "name" : "Debjeet Ghosh",
+    "subjects": {
+        "Math":74,
+        "Phy":16,
+        "Chem":63,
+    }
+}
+
+print(Studentdetails)
+print(Studentdetails["name"])
+print(Studentdetails["subjects"]["Phy"])
+
+
