e5f4ca93b8c1b1ebff66583a2aa5f797b77a1fdc
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-25 11:30:04 +0530

    Updating Loops Folder

---
+++ Loops/while_basics.py
@@ -0,0 +1,19 @@
+count = 1
+while count<=5:
+    print("Hello",count)
+    count = count + 1
+print(count)
+
+
+
+i = 1
+while i<=5:
+    print(i)
+    i += 1
+
+i = 5
+while i>0:
+    print(i)
+    i -= 1
+
+print("Loop Ended")
