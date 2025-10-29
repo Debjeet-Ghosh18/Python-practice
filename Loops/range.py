cd30859c2cc4e3182476ef89fd9972accb292907
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-29 08:37:19 +0530

    Update

--- Loops/range.py
+++ Loops/range.py
@@ -1,11 +1,14 @@
-seq = range(5)
+# seq = range(5)
 
-print(seq[0])
-print(seq[1])
-print(seq[2])
-print(seq[3])
-print(seq[4])
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
 
 
-for i in range(10):
+# for i in range(10):
+#     print(i)
+
+for i in range(1,10,2):
     print(i)
