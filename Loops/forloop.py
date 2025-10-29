227b2dada0f266b47b8298e3bfce7346ac2d423e
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-10-29 08:15:50 +0530

    Update

---
+++ Loops/forloop.py
@@ -0,0 +1,12 @@
+nums = [1,2,3,4,5,6,7,8,9]
+
+for val in nums:
+    print(val)
+
+names = ["Virat","Rohit","Yuvi","Dhoni","Gambir","Sikhar"]
+
+for hello in names:
+    print(hello)
+
+for char in hello:
+    print(char)
