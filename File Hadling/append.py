93b7016ed1e9aadcd0a559c2b28beed24bfde4cc
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-11-24 09:38:44 +0530

    Update

---
+++ File Hadling/append.py
@@ -0,0 +1,6 @@
+f = open("File Hadling\\demo.txt","a")
+
+f.write("\nHard work always pays off")
+
+f.close()
+
---
+++ File Hadling/overwrite.py
@@ -0,0 +1,5 @@
+f = open("File Hadling\\demo.txt","w")
+
+f.write("I need to keep going")
+
+f.close()
