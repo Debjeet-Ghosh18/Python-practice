bded1a9b24c2e56dfee176c8ce28486d9cce6f73
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-11-24 09:45:55 +0530

    Update

--- File Hadling/overwrite.py
+++ File Hadling/overwrite.py
@@ -3,3 +3,8 @@ f = open("File Hadling\\demo.txt","w")
 f.write("I need to keep going")
 
 f.close()
+
+f = open("File Hadling\\demo.txt","r+") #when we overwrite from the begginig then we use a+
+
+f.write("heyee")
+print(f.read())
