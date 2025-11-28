f172977b4cb33be6ea6491e654d8133e220a26eb
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-11-28 12:32:31 +0530

    Updating OOPS Folder

---
+++ MiniProject/Project.py
@@ -0,0 +1,25 @@
+import random
+
+Target = random.randint(1,100)
+Target  = 40
+while True:
+    userChoice = input("Guess the number or Quiet(Q): ")
+
+    if(userChoice == "Q"):
+        break
+
+    userChoice = int(userChoice)
+    if(userChoice == Target):
+        print("Sucess: Correct Guess")
+        break
+
+    elif(userChoice<Target):
+        print("Your Number is too small take a bigger guess")
+
+    elif(userChoice>Target):
+        print("Your number is too big take a smaller guess")
+
+
+print("---GAME OVER---")
+
+
