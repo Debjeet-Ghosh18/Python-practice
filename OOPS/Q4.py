25b585e1ba984fe7657c091d7fbfd2a0f2895b20
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-11-28 12:10:53 +0530

    Updating OOPS Folder

---
+++ OOPS/Q4.py
@@ -0,0 +1,20 @@
+class Employee:
+    def __init__(self,role,dept,salary):
+        self.role = role
+        self.dept = dept
+        self.salary = salary
+
+    def ShowDetails(self):
+        print("Role",self.role)
+        print("Department",self.dept)
+        print("Salary",self.salary)
+
+class Engineer(Employee):
+    def __init__(self, name ,age):
+        self.name = name 
+        self.age = age
+        super().__init__("Data Scientist","IT","40000")
+
+e1 = Engineer("Debjeet","20")
+e1.ShowDetails()
+        
