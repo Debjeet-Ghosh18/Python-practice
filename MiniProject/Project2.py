61c0172cb17ba4b0be2c7cccb82b08827cb32cd7
Debjeet Ghosh <debjeetghosh283@gmail.com>
2025-11-28 12:49:35 +0530

    Updating OOPS Folder

--- MiniProject/Project2.py
+++ MiniProject/Project2.py
@@ -3,15 +3,41 @@
 import random
 import string
 
+# print(type(string.ascii_letters))
+# print(string.ascii_lowercase)
+# print(string.ascii_uppercase)
+# print(string.ascii_letters)
+# print(string.punctuation)
+# print(string.digits)
+# val = random.choice([1,2,3])
 
-print(type(string.ascii_letters))
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.ascii_letters)
-print(string.punctuation)
-print(string.digits)
-val = random.choice([1,2,3])
-print(val)
+pass_len = 12
+charValues = string.ascii_letters + string.punctuation + string.digits
+
+#List Comprehension
+# res = "".join([random.choice(charValues) for i in range(pass_len)])
+# print(res)
+
+password = ""
+for i in range(pass_len):
+   password += random.choice(charValues)
+
+print("Your Random Password is ",password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
